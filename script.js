3e1f3a6b54fcf1e06fc5cd7c878da92ce247eda5
RoanMacmillan <hibee94@gmail.com>
2022-08-25 17:44:59 +0100

    added real time updates

--- script.js
+++ script.js
@@ -7,53 +7,74 @@ const continueButton = document.getElementById("btn2");
 const completeForm = document.getElementById("myform");
 const secondState = document.getElementById("second-state");
 
+const nameInput = document.getElementById("fname");
+const nameError = document.getElementById("name-error");
 
-confirmButton.addEventListener("click", validateForm);
-continueButton.addEventListener("click", refreshPage);
+const cvcInput = document.getElementById("cvc");
+const cvcError = document.getElementById("cvc-error");
+
+const mmInput = document.getElementById("expmm");
+const yyInput = document.getElementById("expyy");
+const expError = document.getElementById("exp-error");
+
+const cardInput = document.getElementById("cnumber");
+const cardError = document.getElementById("number-error");
+
+const credNum = document.getElementById("cred-number");
+const cardName = document.getElementById("card-name");
+const cvcNum = document.getElementById("cvc-num");
+const yyNum = document.getElementById("yy-num");
+const mmNum = document.getElementById("mm-num");
+
+
+
 
 let errorName = 0, errorNum = 0, errorMM = 0, errorYY = 0, errorCVC = 0;
 
 
-    //returns to form page after completion
+//returns to form page after completion
 
-    function refreshPage() {
-
-
-        document.location.reload();
-
-    }
+function refreshPage() {
 
 
-    // submit completed form
+    document.location.reload();
 
-    function submitForm() {
-
-        completeForm.style.display = "none";
-        secondState.style.display = "flex";
-
-    }
+}
 
 
-    function validateCred(no) {
+// submit completed form
 
-        let cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+function submitForm() {
 
-        return cardno.test(String(no).toLowerCase());
+    completeForm.style.display = "none";
+    secondState.style.display = "flex";
+
+}
+
+// regex for major card numbers
+
+function validateCred(input) {
+
+    let cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+
+    return input.value.match(cardno);
 
 
-    }
+}
 
 
-    // form validation
+confirmButton.addEventListener("click", validateForm);
+continueButton.addEventListener("click", refreshPage);
 
-    function validateForm() {
+
+// form validation
+
+function validateForm() {
 
 
 
     // Checks if name is not blank 
 
-    const nameInput = document.getElementById("fname");
-    const nameError = document.getElementById("name-error");
 
     if (nameInput.value === '') {
 
@@ -70,8 +91,7 @@ let errorName = 0, errorNum = 0, errorMM = 0, errorYY = 0, errorCVC = 0;
 
     // Checks if cvc is a number and correct format
 
-    const cvcInput = document.getElementById("cvc");
-    const cvcError = document.getElementById("cvc-error");
+
 
     if (cvcInput.value === '') {
 
@@ -97,9 +117,7 @@ let errorName = 0, errorNum = 0, errorMM = 0, errorYY = 0, errorCVC = 0;
 
     // Checks if expiry date is a number and correct format
 
-    const mmInput = document.getElementById("expmm");
-    const yyInput = document.getElementById("expyy");
-    const expError = document.getElementById("exp-error");
+
 
     if (mmInput.value === '' || yyInput.value === '') {
 
@@ -130,27 +148,22 @@ let errorName = 0, errorNum = 0, errorMM = 0, errorYY = 0, errorCVC = 0;
     }
 
 
-    // Checks if credit card entry is a number and 16 digits
+    // Checks for valid card entry
 
-    const cardInput = document.getElementById("cnumber");
-    const cardError = document.getElementById("number-error");
 
-    if (cardInput.value === '' || cardInput.value.length < 16) {
 
-        cardError.innerHTML = "Can't be blank or less than 16 digits";
+    if (!validateCred(cardInput)) {
+
+        cardError.innerHTML = "Please enter a valid card number";
         cardError.style.display = "block";
         cardInput.style.borderColor = "red";
         errorNum++;
 
-    } else if (isNaN(cardInput.value)) {
+    }
 
 
-        cardError.innerHTML = "Wrong format, numbers only";
-        cardError.style.display = "block";
-        cardInput.style.borderColor = "red";
-        errorNum++;
 
-    } else {
+    else {
 
         cardError.style.display = "none";
         errorNum = 0;
@@ -170,5 +183,50 @@ let errorName = 0, errorNum = 0, errorMM = 0, errorYY = 0, errorCVC = 0;
 }
 
 
+// card details update in realtime 
+
+nameInput.addEventListener("keyup", nameUpdate);
+cardInput.addEventListener("keyup", numUpdate);
+cvcInput.addEventListener("keyup", cvcUpdate);
+yyInput.addEventListener("keyup", yyUpdate);
+mmInput.addEventListener("keyup", mmUpdate);
+
+function numUpdate() {
+
+
+
+    let space = cardInput.value;
+    let numChange = space.match(/.{1,4}/g);
+    credNum.innerHTML = numChange.join(' ');
+
+
+
+}
+
+function nameUpdate() {
+
+
+    cardName.innerHTML = nameInput.value;
+
+
+}
+
+function cvcUpdate() {
+
+    cvcNum.innerHTML = cvcInput.value;
+
+}
+
+function yyUpdate() {
+
+    yyNum.innerHTML = yyInput.value;
+
+}
+
+function mmUpdate() {
+
+    mmNum.innerHTML = mmInput.value;
+
+}
 
 
